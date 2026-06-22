87a11c3ad50998b1d05456a569344cfacbbf6d34
GedeKay <jikaj546@gmail.com>
2026-06-22 13:09:02 +0200

    refactor(RouteAttributesServiceProvider): extraction de la logique de scan des contrôleurs dans une méthode dédiée et correction du namespace en chaîne PHP.

--- src/RouteAttributesServiceProvider.php
+++ src/RouteAttributesServiceProvider.php
@@ -8,9 +8,14 @@ class RouteAttributesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->scanControllers();
+    }
+
+    protected function scanControllers(): void
+    {
         RouteScanner::scan(
             app_path('Http/Controllers'),
-            'App\Http\Controllers'
+            'App\\Http\\Controllers'
         );
     }
 }
