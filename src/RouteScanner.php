f75e2b6767b855cb638e71954ef83a24e41aaade
GedeKay <jikaj546@gmail.com>
2026-07-01 14:49:59 +0200

    refactor(scanner)ajout du prefixe et middleware

--- src/RouteScanner.php
+++ src/RouteScanner.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace RouteAttributes;
 
@@ -9,6 +9,8 @@ use RouteAttributes\Attributes\Post;
 use RouteAttributes\Attributes\Put;
 use RouteAttributes\Attributes\Patch;
 use RouteAttributes\Attributes\Delete;
+use RouteAttributes\Attributes\Prefix;
+use RouteAttributes\Attributes\Middleware;
 
 class RouteScanner
 {
@@ -24,14 +26,12 @@ class RouteScanner
                 continue;
             }
 
-            
             $class = str_replace(
                 [$path, '/', '.php'],
                 [$baseNamespace, '\\', ''],
                 $file->getPathname()
             );
 
-          
             $class = preg_replace('/\\\\+/', '\\', $class);
 
             if (!class_exists($class)) {
@@ -40,45 +40,91 @@ class RouteScanner
 
             $reflection = new ReflectionClass($class);
 
+            $classPrefix = null;
+            $classMiddleware = [];
+
+            foreach ($reflection->getAttributes(Prefix::class) as $attribute) {
+                $prefixInstance = $attribute->newInstance();
+                $classPrefix = $prefixInstance->uri;
+            }
+
+            foreach ($reflection->getAttributes(Middleware::class) as $attribute) {
+                $middlewareInstance = $attribute->newInstance();
+                $classMiddleware = array_merge($classMiddleware, (array) $middlewareInstance->middleware);
+            }
+
             foreach ($reflection->getMethods() as $method) {
+                $methodPrefix = $classPrefix;
+                $methodMiddleware = $classMiddleware;
 
-                // GET
-                foreach ($method->getAttributes(Get::class) as $attribute) {
-                    $route = $attribute->newInstance();
-
-                    Route::get($route->uri, [$class, $method->getName()])
-                        ->name($route->name);
+                foreach ($method->getAttributes(Prefix::class) as $attribute) {
+                    $prefixInstance = $attribute->newInstance();
+                    $methodPrefix = $prefixInstance->uri;
                 }
 
-                // POST
+                foreach ($method->getAttributes(Middleware::class) as $attribute) {
+                    $middlewareInstance = $attribute->newInstance();
+                    $methodMiddleware = array_merge($methodMiddleware, (array) $middlewareInstance->middleware);
+                }
+
+                $uri = $methodPrefix ? $methodPrefix . '/' . $this->getMethodUri($method) : $this->getMethodUri($method);
+
+                foreach ($method->getAttributes(Get::class) as $attribute) {
+                    $route = $attribute->newInstance();
+                    $finalUri = $methodPrefix ? $methodPrefix . '/' . ltrim($route->uri, '/') : $route->uri;
+
+                    Route::get($finalUri, [$class, $method->getName()])
+                        ->name($route->name)
+                        ->middleware($methodMiddleware);
+                }
+
                 foreach ($method->getAttributes(Post::class) as $attribute) {
                     $route = $attribute->newInstance();
+                    $finalUri = $methodPrefix ? $methodPrefix . '/' . ltrim($route->uri, '/') : $route->uri;
 
-                    Route::post($route->uri, [$class, $method->getName()])
-                        ->name($route->name);
+                    Route::post($finalUri, [$class, $method->getName()])
+                        ->name($route->name)
+                        ->middleware($methodMiddleware);
                 }
 
                 foreach ($method->getAttributes(Put::class) as $attribute) {
                     $route = $attribute->newInstance();
+                    $finalUri = $methodPrefix ? $methodPrefix . '/' . ltrim($route->uri, '/') : $route->uri;
 
-                    Route::put($route->uri, [$class, $method->getName()])
-                        ->name($route->name);
+                    Route::put($finalUri, [$class, $method->getName()])
+                        ->name($route->name)
+                        ->middleware($methodMiddleware);
                 }
 
                 foreach ($method->getAttributes(Patch::class) as $attribute) {
                     $route = $attribute->newInstance();
+                    $finalUri = $methodPrefix ? $methodPrefix . '/' . ltrim($route->uri, '/') : $route->uri;
 
-                    Route::patch($route->uri, [$class, $method->getName()])
-                        ->name($route->name);
+                    Route::patch($finalUri, [$class, $method->getName()])
+                        ->name($route->name)
+                        ->middleware($methodMiddleware);
                 }
 
                 foreach ($method->getAttributes(Delete::class) as $attribute) {
                     $route = $attribute->newInstance();
+                    $finalUri = $methodPrefix ? $methodPrefix . '/' . ltrim($route->uri, '/') : $route->uri;
 
-                    Route::delete($route->uri, [$class, $method->getName()])
-                        ->name($route->name);
+                    Route::delete($finalUri, [$class, $method->getName()])
+                        ->name($route->name)
+                        ->middleware($methodMiddleware);
                 }
             }
         }
+    }
+
+    private static function getMethodUri(\ReflectionMethod $method): string
+    {
+        foreach ($method->getAttributes() as $attribute) {
+            $instance = $attribute->newInstance();
+            if (property_exists($instance, 'uri')) {
+                return $instance->uri;
+            }
+        }
+        return '';
     }
 }
